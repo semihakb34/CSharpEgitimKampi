ab7246ee7b509b2ed5e6348a94e130e8aca4d09e
semih <semih@DESKTOP-FOCU0IB>
2025-12-10 15:06:49 +0300

    3. Ders

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region ifElseYapısı
+
+            // 1. DENEME
+
+
+
+            //Console.Write("Lütfen Şifreyi Giriniz: ");
+            //string password;
+            //password = Console.ReadLine();
+            //if (password == "admin123")
+            //{
+            //       Console.WriteLine("Giriş Başarılı! Hoşgeldiniz.");
+            //}
+            //else
+            //{
+            //       Console.WriteLine("Giriş Başarısız! Lütfen bilgilerinizi kontrol ediniz.");
+            //}
+            //       Console.Read();
+
+
+            // 2. DENEME & VE anlamına gelen operatör
+
+            //string capital, country;
+            //Console.Write("Lütfen Bir Başkent Adı Giriniz: ");
+            //capital = Console.ReadLine();
+
+
+            //Console.Write("Lütfen Ülke Adı Giriniz ");
+            //country = Console.ReadLine();
+
+
+            //if (capital == "istanbul" & country == "türkiye")
+            //{
+            //    Console.Write("Veriler Dogrulandı.");
+            //}
+            //else
+            //{
+            //    Console.Write("Hatalı Bilgi");
+            //}
+
+            //Console.Read();
+
+
+            // 3. DENEME == eşittir operatörü
+
+            //int number;
+            //Console.Write("Lütfen Bir Sayı Giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //if (number == 29)
+            //{
+            //    Console.Write("Sayı Dogru ");
+            //}
+            //else
+            //{
+            //    Console.Write("Sayı Hatalı ");
+            //}
+
+            //Console.Read();
+
+
+
+
+            // sınav ortalamasına göre ögrenci sınıflandırması
+            // 4. DENEME > < = operatörleri 
+
+            //int exam1, exam2, exam3, result;
+
+            //Console.Write("Lütfen 1. Sınav Notunu Giriniz: ");
+            //exam1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen 2. Sınav Notunu Giriniz: ");
+            //exam2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen 3. Sınav Notunu Giriniz: ");
+            //exam3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("");
+
+            //result = (exam1 + exam2 + exam3) / 3;
+            //Console.WriteLine("Sınavların Ortalaması: " + result);
+
+            //Console.WriteLine("");
+
+            //if (result > 0 &  result <= 25)
+            //{
+            //    Console.Write("Çok Kötü Bir Ögrenci ");
+
+            //}
+            //if (result > 26 & result <= 49)
+            //{
+            //    Console.Write("Kötü Bir Ögrenci ");
+            //}
+            //if (result > 50 & result <= 69)
+            //{
+            //    Console.Write("Ortalama Bir Ögrenci ");
+            //}
+            //if (result > 70 & result <= 85)
+            //{
+            //    Console.Write("İyi Bir Ögrenci ");
+            //}
+            //if (result >=86)
+            //{
+            //    Console.Write("Mükemmel Bir Ögrenci ");
+            //}
+
+            ////Console.WriteLine(result);
+
+            //    Console.Read();
+
+
+            // 5. DENEME || veya operatörü. | veya anlamına gelir.
+
+            //string city;
+
+            //Console.Write("Lütfen Bir Şehir Giriniz: ");
+            //city = Console.ReadLine();
+            //city = city.ToLower(); // Verilen cevapların hepsini kücük harfe dönüştüren kod..
+
+            //if (city == "istanbul" || city == "ankara" || city == "izmir" || city == "bursa")
+
+            //{
+            //    Console.WriteLine("Şehir Mevcut");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şehir Mevcut Degil");
+            //}
+
+            //Console.Read();
+
+            // 6. DENEME != eşit değildir operatörü. 
+
+            //Console.Write("Bir Kullanıcı Adı Giriniz: ");
+            //string username = (Console.ReadLine());
+            //if (username != "admin")
+            //{
+            //    Console.WriteLine("Giriş Başarısız");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hoşgeldiniz");
+            //}
+
+            //Console.Read();
+
+            #endregion
+
+        }
+    }
+}
