2775069661414440009eedcf005a51e5df6f3fad
semih <semih@DESKTOP-FOCU0IB>
2025-12-14 03:42:41 +0300

    5. Ders

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane * yazdırma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //     Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan yana 10 tane * yazdırma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //     Console.Write(" * ");
+            //}
+
+            #endregion
+
+            #region Alt alta 10 tane yıldız oluşturma her satırda 10 adette yıldız olacak şekilde
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("* * * * * * * * * *");
+            //}
+
+            #endregion
+
+            #region Dik Üçgen Yıldız
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Üçgen Yıldız
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Üçgen ve Ters Üçgenin Birleşimi
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int l = 1; l <= k; l++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region Baklava Dilimi
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Piramit
+
+            //int n = 5;
+
+            //    Boşluklar için kod alanı
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int k = n - i; k > 0; k--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //     Yıldızların kod alanı
+            //    for (int j = 1; j <= 2 * i - 1; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            #endregion,
+
+            #region Ters Piramit
+
+            //int n = 5;
+            //    // Boşluklar
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    // Yıldızlar
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+        }
+    }
+}
---
+++ 05_LoopsWithStars/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("05_LoopsWithStars")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("05_LoopsWithStars")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07d07153-05a7-4d7e-b96f-92e20b6043e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
